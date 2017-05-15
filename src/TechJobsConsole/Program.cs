f44e094b3e515e0d5fe0111ce25565f83e0bd49d
Erik Bates <erik.s.bates@gmail.com>
2017-05-14 21:45:46 -0500

    Implemented PrintJobs method.

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -118,7 +118,22 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            if (someJobs.Count > 0)
+            {
+                for (int i = 0; i < someJobs.Count; i++)
+                {
+                    Console.WriteLine("**********");
+                    foreach (KeyValuePair<string, string> job in someJobs[i])
+                    {
+                        Console.WriteLine(job.Key + ": " + job.Value);
+                    }
+                    Console.WriteLine("**********");
+                }
+            }      
+            else
+            {
+                Console.WriteLine("**************\nNo jobs found!\n**************");
+            }
         }
     }
 }
